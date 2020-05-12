93051adb0d3e1eb4403ee4cc991832a3b774f8a3
kwamkert <tomriddle477@gmail.com>
2020-05-12 22:34:40 +0000

    cors config added

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
-
+from flask import request
+from flask_cors import CORS, cross_origin
 from tensorflow.keras.models import load_model
 
 
@@ -10,16 +11,39 @@ import numpy as np
 
 
 app = Flask(__name__);
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 def init():
 
     #define global variables
     global model
-
+    global lists
     #import model
-    model = load_model('AlexNetModel.hdf5')
+    model = load_model('../cnn/AlexNetModel.hdf5')
     lists = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 
 
 
+@app.route("/", methods=['POST'])
+def index():
+    new_image  =  request.files['file'].stream
+    new_img = image.load_img(new_image, target_size=(224, 224))
+    img = image.img_to_array(new_img)
+    img = np.expand_dims(img, axis=0)
+    img = img/255
+    prediction = model.predict(img)
+
+    d = prediction.flatten()
+    j = d.max()
+    for index,item in enumerate(d):
+        if item == j:
+            class_name = lists[index]
+    return '<h1>'+class_name+' </h1>'
+
+
+if __name__ == '__main__':
+    init()
+    app.run()
 
