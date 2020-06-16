bf60b9b00f9f10997dee1623c0e2661b57737a4f
kwamkert <tomriddle477@gmail.com>
2020-06-16 22:43:25 +0000

    modified project

--- project.py
+++ project.py
@@ -10,6 +10,7 @@ from tensorflow.keras.preprocessing import image
 
 import numpy as np
 
+from diseases import * 
 
 app = Flask(__name__);
 cors = CORS(app)
@@ -21,10 +22,14 @@ def init():
     #define global variables
     global model
     global lists
+    global remedies
     #import model
     model = load_model('../cnn/AlexNetModel.hdf5')
-    lists = ['Apple___Apple scab', 'Apple___Black rot', 'Apple___Cedar apple rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry(including sour)___Powdery mildew', 'Cherry(including sour)___healthy', 'Corn (maize)___Cercospora leaf spot Gray leaf spot', 'Corn (maize)___Common rust ', 'Corn (maize)___Northern Leaf Blight', 'Corn (maize)___healthy', 'Grape___Black rot', 'Grape___Esca (Black Measles)', 'Grape___Leaf blight (Isariopsis Leaf Spot)', 'Grape___healthy', 'Orange___Haunglongbing (Citrus greening)', 'Peach___Bacterial spot', 'Peach___healthy', 'Pepper bell___Bacterial spot', 'Pepper ___healthy', 'Potato___Early blight', 'Potato___Late blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early blight', 'Tomato___Late blight', 'Tomato___Leaf Mold', 'Tomato___Septoria leaf spot', 'Tomato___Spider mites Two spotted spider mite', 'Tomato___Target Spot', 'Tomato___Tomato Yellow Leaf Curl Virus', 'Tomato___Tomato mosaic virus', 'Tomato___healthy']
+    
+    lists = load_diseases()
+    remedies = load_remedies()
 
+   
 
 
 @app.route("/", methods=['POST'])
@@ -41,10 +46,12 @@ def index():
     for index,item in enumerate(d):
         if item == j:
             class_name = lists[index]
+            remedy = remedies[index]
     response = class_name.split("___")
     return jsonify(
             plant=response[0],
-            status=response[1]
+            status=response[1],
+            remedy=remedy
                 );
 
 
